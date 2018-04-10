a38e65636ca3197353df0ea613e84d7afd456538
johanc.suarez <johanc.suarez@udea.edu.co>
2018-04-10 17:58:28 -0500

    "Login y registro funcionando"

--- Lab2Activities/src/main/java/co/edu/udea/compumovil/gr05_20181/lab2/RegistroActivity.java
+++ Lab2Activities/src/main/java/co/edu/udea/compumovil/gr05_20181/lab2/RegistroActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +19,8 @@ import java.util.ArrayList;
 import co.edu.udea.compumovil.gr05_20181.lab2.data.dbHelper;
 import co.edu.udea.compumovil.gr05_20181.lab2.data.usuario;
 import gun0912.tedbottompicker.TedBottomPicker;
+
+import static android.util.Log.println;
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -36,7 +39,7 @@ public class RegistroActivity extends AppCompatActivity {
         botonImagen = findViewById(R.id.botonImagen);
         campoNombre=findViewById(R.id.campoNombre);
         campoApellido=findViewById(R.id.campoApellido);
-        campoContraseña=findViewById(R.id.campoNombre);
+        campoContraseña=findViewById(R.id.campoContraseña);
         campoCorreo=findViewById(R.id.campoEmail);
 
 
@@ -44,16 +47,23 @@ public class RegistroActivity extends AppCompatActivity {
        botonGuardar.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
-
-               String foto=datosRecuperados;
+//hola@hotmail.com contraseña: carla
+               String foto = "x";
                String nombre, correo, contraseña, apellido;
-               nombre= String.valueOf(campoNombre.getText());
-               apellido= String.valueOf(campoApellido.getText());
-               correo= String.valueOf(campoCorreo.getText());
-               contraseña= String.valueOf(campoContraseña.getText());
-               usuario user=new usuario(nombre,contraseña,apellido,correo,foto);
-               dbHelper db=new dbHelper(getApplicationContext());
-               db.guardarUsuario(user);
+               nombre = String.valueOf(campoNombre.getText());
+               apellido = String.valueOf(campoApellido.getText());
+               correo = String.valueOf(campoCorreo.getText());
+               contraseña = String.valueOf(campoContraseña.getText());
+               usuario user = new usuario(nombre, contraseña, apellido, correo, foto);
+               dbHelper db = new dbHelper(getApplicationContext());
+               try  {
+                   println(Log.ERROR,"MYTAG","Error "+contraseña);
+                   db.guardarUsuario(user);
+               }
+               catch (Exception e){
+                  println(Log.ERROR,"MYTAG","Error "+e);
+               }
+
            }
        });
 
